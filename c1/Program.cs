c51074ff3176e458662c6fdfc0638b4ff4259f18
Enrico Sada <enrico@sada.io>
2017-12-14 00:27:14 +0100

    add proj

---
+++ c1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace c1
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public decimal Price { get; set; }
+        public string[] Sizes { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var  product = new Product();
+            
+            product.Name = "Apple";
+            product.ExpiryDate = new DateTime(2008, 12, 28);
+            product.Price = 3.99M;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+            string json = JsonConvert.SerializeObject(product);
+            Console.WriteLine(json);
+        }
+    }
+}
